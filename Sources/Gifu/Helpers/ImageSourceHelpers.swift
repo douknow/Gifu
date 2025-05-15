f1e193272afe9f14a19a71cf24948fae955609f3
cry <saynotta@gmail.com>
2025-05-15 21:45:51 +0800

    fix: 支持apng

--- Sources/Gifu/Helpers/ImageSourceHelpers.swift
+++ Sources/Gifu/Helpers/ImageSourceHelpers.swift
@@ -3,8 +3,6 @@ import ImageIO
 import MobileCoreServices
 import UIKit
 
-typealias GIFProperties = [String: Double]
-
 /// Most GIFs run between 15 and 24 Frames per second.
 ///
 /// If a GIF does not have (frame-)durations stored in its metadata,
@@ -26,12 +24,14 @@ private let minFrameDuration: Double = 0.01
 func CGImageFrameDuration(with imageSource: CGImageSource, atIndex index: Int) -> TimeInterval {
   guard imageSource.isAnimatedGIF else { return 0.0 }
   
-  // Return nil, if the properties do not store a FrameDuration or FrameDuration <= 0
-  guard let GIFProperties = imageSource.properties(at: index),
-        let duration = frameDuration(with: GIFProperties),
-        duration > 0 else { return defaultFrameDuration }
+    // Return nil, if the properties do not store a FrameDuration or FrameDuration <= 0
+    guard let duration = imageSource.duration(at: index),
+          duration > 0
+    else {
+        return defaultFrameDuration
+    }
   
-  return capDuration(with: duration)
+    return capDuration(with: duration)
 }
 
 /// Ensures that a duration is never smaller than a threshold value.
@@ -42,27 +42,8 @@ func capDuration(with duration: Double) -> Double {
   return cappedDuration
 }
 
-/// Returns a frame duration from a `GIFProperties` dictionary.
-///
-/// - returns: A frame duration.
-func frameDuration(with properties: GIFProperties) -> Double? {
-  guard let unclampedDelayTime = properties[String(kCGImagePropertyGIFUnclampedDelayTime)],
-        let delayTime = properties[String(kCGImagePropertyGIFDelayTime)]
-  else { return nil }
-  
-  return duration(withUnclampedTime: unclampedDelayTime, andClampedTime: delayTime)
-}
-
-/// Calculates frame duration based on both clamped and unclamped times.
-///
-/// - returns: A frame duration.
-func duration(withUnclampedTime unclampedDelayTime: Double, andClampedTime delayTime: Double) -> Double? {
-  let delayArray = [unclampedDelayTime, delayTime]
-  return delayArray.filter({ $0 >= 0 }).first
-}
-
 /// An extension of `CGImageSourceRef` that adds GIF introspection and easier property retrieval.
-extension CGImageSource {
+public extension CGImageSource {
 
     var isTypeGIF: Bool {
         UTTypeConformsTo(CGImageSourceGetType(self) ?? "" as CFString, kUTTypeGIF)
@@ -71,35 +52,72 @@ extension CGImageSource {
     var isTypeHEICS: Bool {
         UTTypeConformsTo(CGImageSourceGetType(self) ?? "" as CFString, "public.heics" as CFString)
     }
+    
+    var isTypeAPNG: Bool {
+        UTTypeConformsTo(CGImageSourceGetType(self) ?? "" as CFString, kUTTypePNG)
+    }
   
-  /// Returns whether the image source contains an animated GIF.
-  ///
-  /// - returns: A boolean value that is `true` if the image source contains animated GIF data.
-  var isAnimatedGIF: Bool {
-    let imageCount = CGImageSourceGetCount(self)
-    return imageCount > 1
-  }
-  
-  /// Returns the GIF properties at a specific index.
-  ///
-  /// - parameter index: The index of the GIF properties to retrieve.
-  /// - returns: A dictionary containing the GIF properties at the passed in index.
-  func properties(at index: Int) -> GIFProperties? {
-      CGImageSourceGetType(self)
-      if isTypeGIF {
-          guard let imageProperties = CGImageSourceCopyPropertiesAtIndex(self, index, nil) as? [String: AnyObject] else { return nil }
-          return imageProperties[String(kCGImagePropertyGIFDictionary)] as? GIFProperties
-      } else if isTypeHEICS {
-          guard let imageProperties = CGImageSourceCopyPropertiesAtIndex(self, index, nil) as? [String: AnyObject] else { return nil }
-          if #available(iOS 13.0, *) {
-              return imageProperties[String(kCGImagePropertyHEICSDictionary)] as? GIFProperties
-          } else {
-              return nil
-          }
-      } else {
-          return nil
-      }
-  }
+    /// Returns whether the image source contains an animated GIF.
+    ///
+    /// - returns: A boolean value that is `true` if the image source contains animated GIF data.
+    var isAnimatedGIF: Bool {
+        let imageCount = CGImageSourceGetCount(self)
+        return imageCount > 1
+    }
+    
+    func duration(at index: Int) -> TimeInterval? {
+        guard let imageProperties = CGImageSourceCopyPropertiesAtIndex(self, index, nil) as? [CFString: AnyObject] else {
+            return nil
+        }
+         
+        return duration(imageProperties: imageProperties)
+    }
+    
+    func duration(imageProperties: [CFString: Any]) -> TimeInterval? {
+        if isTypeGIF {
+            guard let gifProperties = imageProperties[kCGImagePropertyGIFDictionary] as? [CFString: Any] else {
+                return nil
+            }
+            
+            return duration(
+                withUnclampedTime: gifProperties[kCGImagePropertyGIFUnclampedDelayTime] as? TimeInterval,
+                andClampedTime: gifProperties[kCGImagePropertyGIFDelayTime] as? TimeInterval
+            )
+        } else if isTypeHEICS {
+            if #available(iOS 13.0, *) {
+                guard let heicsProperties = imageProperties[kCGImagePropertyHEICSDictionary] as? [CFString: Any] else {
+                    return nil
+                }
+                
+                return duration(
+                    withUnclampedTime: heicsProperties[kCGImagePropertyHEICSUnclampedDelayTime] as? TimeInterval,
+                    andClampedTime: heicsProperties[kCGImagePropertyHEICSDelayTime] as? TimeInterval
+                )
+            } else {
+                return nil
+            }
+        } else if isTypeAPNG {
+            guard let apngProperties = imageProperties[kCGImagePropertyPNGDictionary] as? [CFString: Any] else {
+                return nil
+            }
+            
+            return duration(
+                withUnclampedTime: apngProperties[kCGImagePropertyAPNGUnclampedDelayTime] as? TimeInterval,
+                andClampedTime: apngProperties[kCGImagePropertyAPNGDelayTime] as? TimeInterval
+            )
+        } else {
+            return nil
+        }
+    }
+    
+    /// Calculates frame duration based on both clamped and unclamped times.
+    ///
+    /// - returns: A frame duration.
+    func duration(withUnclampedTime unclampedDelayTime: Double?, andClampedTime delayTime: Double?) -> TimeInterval? {
+        let delayArray = [unclampedDelayTime, delayTime]
+        return delayArray.compactMap { $0 }.filter({ $0 >= 0 }).first
+    }
+    
 }
 
 #endif
